d36242b1a5e1e88ef2ca95cd25b46f72c234ba51
webdev-html <73722228+webdev-html@users.noreply.github.com>
2020-11-11 17:55:34 -0500

    Add files via upload

--- website/js scripts/menubar.js
+++ website/js scripts/menubar.js
@@ -13,6 +13,8 @@ function frame3() {
 function finalframe() {
     document.getElementById("navbaricon").src = "/pictures/finalframe.png"
 }
+
+
 function navbarcontactpage() {
     document.getElementById("body").style.background = "lightseagreen";
     document.getElementById("homepage").innerHTML = "Home";
@@ -22,11 +24,10 @@ function navbarcontactpage() {
     document.getElementById("navbaricon").style.cursor = "auto"
     document.getElementById("port").innerHTML = "portfolio"
 
-    setTimeout(frame, 50)
-    setTimeout(frame2, 100)
-    setTimeout(frame3, 150)
-    setTimeout(finalframe, 200)
-    
+    setTimeout(frame, 35)
+    setTimeout(frame2, 70)
+    setTimeout(frame3, 105)
+    setTimeout(finalframe, 140)
     
 }
 
@@ -38,10 +39,10 @@ function navbarhomepage() {
     document.getElementById("contacts").innerHTML = "contacts and socials"
     document.getElementById("navbaricon").style.cursor = "auto"
     document.getElementById("port").innerHTML = "portfolio"
-    setTimeout(frame, 50)
-    setTimeout(frame2, 100)
-    setTimeout(frame3, 150)
-    setTimeout(finalframe, 200)
+    setTimeout(frame, 35)
+    setTimeout(frame2, 70)
+    setTimeout(frame3, 105)
+    setTimeout(finalframe, 140)
 
     
 }
@@ -54,10 +55,10 @@ function navbaraboutme() {
     document.getElementById("section").innerHTML = ""
     document.getElementById("port").innerHTML = "portfolio"
     document.getElementById("navbaricon").style.cursor = "auto"
-    setTimeout(frame, 50)
-    setTimeout(frame2, 100)
-    setTimeout(frame3, 150)
-    setTimeout(finalframe, 200)
+    setTimeout(frame, 35)
+    setTimeout(frame2, 70)
+    setTimeout(frame3, 105)
+    setTimeout(finalframe, 140)
 }
 
 function navbarport() {
@@ -68,9 +69,9 @@ function navbarport() {
     document.getElementById("section").innerHTML = ""
     document.getElementById("about").innerHTML = "about me"
     document.getElementById("navbaricon").style.cursor = "auto"
-    setTimeout(frame, 50)
-    setTimeout(frame2, 100)
-    setTimeout(frame3, 150)
-    setTimeout(finalframe, 200)
+    setTimeout(frame, 35)
+    setTimeout(frame2, 70)
+    setTimeout(frame3, 105)
+    setTimeout(finalframe, 140)
 
 }
